472696a7580f12e93e34f2ca3e48a1bfa8346d43
yerram viveka <yerram.viveka@hcl.com>
2025-01-15 12:47:22 +0000

    Added sample comment

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,6 +7,7 @@ sap.ui.define(
   
       return BaseController.extend("com.hcl.accountsoperations.controller.App", {
         onInit: function() {
+          //This is a sample comment
         }
       });
     }
